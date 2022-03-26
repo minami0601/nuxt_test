411fd406f77d977a5a99c6b8631ab03057201e7a
minami0601 <minami.love.bb@icloud.com>
2022-03-26 16:24:50 +0900

    middleware編

--- section2/nuxt-test/pages/index.vue
+++ section2/nuxt-test/pages/index.vue
@@ -9,6 +9,19 @@
 
 <script>
 export default {
-  name: 'IndexPage',
+  asyncData( { route, isDev, app, $axios} ){
+    console.log('asyncData')
+    console.log('route.name', route.name)
+    console.log('isDev:', isDev)
+    console.log('app:', app)
+    console.log('app.$axios:', app.$axios)
+    console.log('$axios:', $axios)
+  },
+  created(){
+    console.log('created')
+  },
+  fetch(){
+    console.log('fetch')
+  }
 }
 </script>
